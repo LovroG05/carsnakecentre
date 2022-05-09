21b9048e340c203cf52be29a7be456b9c751f893
LovroG05 <lovro.govekar@gmail.com>
2022-05-09 17:49:51 +0200

    enabling data saving

--- saver.py
+++ saver.py
@@ -3,23 +3,24 @@ from csv import writer
 
 
 class SaveThread(threading.Thread):
-    def __init__(self, threadID, frame):
+    def __init__(self, threadID, frame, steering_value, gas_value, path):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = "SaveThread"
         self.frame = frame
+        self.steering_value = steering_value
+        self.path = path
         
     def run(self):
         print("Starting " + self.name + ": " + str(self.threadID))
         #h,  w = self.frame.shape[:2]
         #newcameramtx, roi=cv2.getOptimalNewCameraMatrix(MAT, DIST, (w, h), 1, (w, h))
         #dst = cv2.undistort(self.frame, MAT, DIST, None, newcameramtx)
-        global STEERING_VALUE, GAS_VALUE, BRAKE_VALUE, DIRECTION, FRAMEPATH
         t = time.time()
-        cv2.imwrite(FRAMEPATH + str(t) + ".jpg", self.frame)
+        cv2.imwrite(self.path + str(t) + ".jpg", self.frame)
         with open("framedata.csv", "a") as file:
             writer_ = writer(file, delimiter=',', quotechar='"')
-            writer_.writerow([FRAMEPATH + str(t) + ".jpg", str(STEERING_VALUE), str(GAS_VALUE), str(BRAKE_VALUE), str(DIRECTION)])
+            writer_.writerow([self.path + str(t) + ".jpg", str(self.steering_value), str(self.gas_value)])
             file.close()
             
         print("Exiting " + self.name + ": " + str(self.threadID))
